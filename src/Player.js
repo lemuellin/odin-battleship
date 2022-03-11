8d1551a30b15b0d35a8b042fd84af88bd3f3fdd3
lemuellin <lemuellin@gmail.com>
2022-03-11 14:31:54 -0800

    Player Factory: players take turns

--- src/Player.js
+++ src/Player.js
@@ -1,23 +1,24 @@
 const Gameboard = require('./Gameboard');
 
-const Player = (name) => {
-    let game = Gameboard();
-    const attack = (enemy, x, y) => {
-        enemy.game.receiveAttack(x, y);
-    }
-    const attackRand = (enemy) => {
+const Player = () => {
+    let gbPlayer = Gameboard();
+    let gbComputer = Gameboard();
+
+    const attack = (x, y) => {
+        gbComputer.receiveAttack(x, y);
+
         while(true){
             let x = Math.floor(Math.random()*9) + 1;
             let y = Math.floor(Math.random()*9) + 1;
 
-            if (game.board[x][y] == null){
-                enemy.game.receiveAttack(x, y);
+            if (gbPlayer.board[x][y] == null){
+                gbPlayer.receiveAttack(x, y);
                 break;
             }
         }
     }
-
-    return{game, attack, attackRand};
+    
+    return{gbPlayer, gbComputer, attack};
 };
 
 module.exports = Player;
--- src/__test__/Player.test.js
+++ src/__test__/Player.test.js
@@ -1,34 +1,31 @@
 const Player = require('../Player');
 
 test('Gameboard factory', () => {
-    const player1 = Player('player');
-    const computer1 = Player('computer');
-    player1.game.placeShip('Carrier', 0, 0, 5, 'vertical');
-    computer1.game.placeShip('Patrol Boat', 5, 5, 2, 'horizontal');
+    const game1 = Player();
+    game1.gbPlayer.placeShip('Carrier', 0, 0, 5, 'vertical');
+    game1.gbComputer.placeShip('Patrol Boat', 5, 5, 2, 'horizontal');
 
-    expect(player1.game.board[0][0][0].getName()).toBe('Carrier');
-    expect(computer1.game.board[5][5][0].getName()).toBe('Patrol Boat');
+    expect(game1.gbPlayer.board[0][0][0].getName()).toBe('Carrier');
+    expect(game1.gbComputer.board[5][5][0].getName()).toBe('Patrol Boat');
 });
 
 test('player attack', () => {
-    const player2 = Player('player');
-    const computer2 = Player('computer');
-    computer2.game.placeShip('Carrier', 3, 4, 5, 'horizontal');
-    player2.attack(computer2, 3, 4);
-    player2.attack(computer2, 3, 5);
-    player2.attack(computer2, 4, 4);
+    const game2 = Player();
+    game2.gbComputer.placeShip('Carrier', 3, 4, 5, 'horizontal');
+    game2.attack(3, 4);
+    game2.attack(3, 5);
+    game2.attack(4, 4);
 
-    expect(computer2.game.board[3][4][2]).toBe('hit');
-    expect(computer2.game.board[4][4]).toBe('miss');
-    expect(computer2.game.board[3][5][2]).toBe('hit');
+    expect(game2.gbComputer.board[3][4][2]).toBe('hit');
+    expect(game2.gbComputer.board[4][4]).toBe('miss');
+    expect(game2.gbComputer.board[3][5][2]).toBe('hit');
 });
 
 test('computer random attack', () => {
-    const player3 = Player('player');
-    const computer3 = Player('computer');
-    player3.game.placeShip('Carrier', 2, 3, 5, 'vertical');
-    computer3.attackRand(player3);
-    console.log(player3.game.board);
+    const game3 = Player();
+    game3.gbPlayer.placeShip('Carrier', 2, 3, 5, 'vertical');
+    game3.attack(0,0);
+    console.log(game3.gbPlayer.board);
+    
     expect('1').toBe('1');
-
 });
